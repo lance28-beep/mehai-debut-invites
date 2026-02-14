28faa7e0f5608c1d3df989d1aee33e6e36885db9
Rolando Valle <vallelance8@gmail.com>
2026-02-14 10:56:21 +0800

    update

--- components/background-music.tsx
+++ components/background-music.tsx
@@ -47,7 +47,7 @@ const BackgroundMusic = () => {
     <audio
       ref={audioRef}
       // Use an encoded URI to avoid issues with spaces/parentheses on some mobile browsers
-      src={encodeURI("/background_music/Kina Grannis ft. Imaginary Future - I Will Spend My Whole Life Loving You (lyrics).mp3")}
+      src={encodeURI("/background_music/Kina Grannis ft. Imaginary Future - I Will Spend My Whole Life Loving You (lyrics) copy.mp3")}
       loop
       preload="auto"
       // playsInline helps iOS treat this as inline media rather than requiring fullscreen behavior
