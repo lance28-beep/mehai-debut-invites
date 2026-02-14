cb89ab526a42f5e6b6a833c42758252d8a354601
Rolando Valle <vallelance8@gmail.com>
2026-02-14 10:34:21 +0800

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import { siteConfig } from "@/content/site"
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://kath-debut-celebration-invitation.vercel.app/"
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://mehai-debut-invites.vercel.app/"
 const canonicalUrl = siteUrl.replace(/\/$/, "")
 const desktopHero = "/Details/linkPreview.jpg"
 const mobileHero = "/Details/linkPreview.jpg"
